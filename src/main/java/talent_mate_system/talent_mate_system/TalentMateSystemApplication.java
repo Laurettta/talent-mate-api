a472feeead1cfa87a5a0b3b39acefd372624d449
Laurettta <okolielauretta0715@gmail.com>
2024-08-12 09:57:36 +0100

    Initial commit

---
+++ src/main/java/talent_mate_system/talent_mate_system/TalentMateSystemApplication.java
@@ -0,0 +1,13 @@
+package talent_mate_system.talent_mate_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentMateSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentMateSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/talent_mate_system/talent_mate_system/TalentMateSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package talent_mate_system.talent_mate_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentMateSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
